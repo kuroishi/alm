e8f4f8289a46ee280cd5ac4bc2c38f86b089088b
KUROISHI Mitsuo <kuroishi@iij.ad.jp>
2020-01-06 16:44:04 +0900

    update.

--- cmd/alm/main.go
+++ cmd/alm/main.go
@@ -34,11 +34,81 @@ func main() {
 	t := time.Now()
 	fmt.Print(t)
 
-	var mon Month
-	mon.Day = make([]Day, 31)
+	var mon []Month
+	mon = make([]Month, 3)
+	mon[0].Day = make([]Day, 31)
+	mon[1].Day = make([]Day, 31)
+	mon[2].Day = make([]Day, 31)
 	fmt.Print("\nmon:\n")
 	//now := time.Now()
-	init_month(mon, time.Date(1976, 9, 1, 0, 0, 0, 0, time.Local))
+	init_month(mon[0], time.Date(1976, 9, 1, 0, 0, 0, 0, time.Local))
+	init_month(mon[1], time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local))
 	alm_view.Display_month(mon)
 	fmt.Print("\n")
 }
+
+/*
+
+	
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "github.com/PuerkitoBio/goquery"
+)
+
+func ScrapeScrape() bool {
+    return true
+}
+
+func Scrape(w string) {
+    query := fmt.Sprintf("https://eow.alc.co.jp/search?q=%s", w)
+    res, err := http.Get(query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    doc, err := goquery.NewDocumentFromReader(res.Body);
+
+    //html, err := doc.Find("body").Html()
+    //fmt.Print(html)
+
+    num, _ := strconv.Atoi(doc.Find("#itemsNumber > strong").Text())
+    if num == 0 {
+        os.Exit(1)
+    }
+
+    word := doc.Find("#resultsList > ul:nth-child(3) > li:nth-child(1) > span:nth-child(1) > h2:nth-child(1) > span:nth-child(1)").Text()
+    word_class := doc.Find("#resultsList > ul:nth-child(3) > li:nth-child(1) .wordclass").Text()
+    fmt.Print(word)
+    fmt.Print(" | ")
+    fmt.Print(word_class)
+    fmt.Print("\n----\n")
+    ScrapeScrape()
+    doc.Find("#resultsList > ul:nth-child(3) > li:nth-child(1) > div:nth-child(2) > ol:nth-child(2) > li").Each(func(idx int, s *goquery.Selection) {
+             idx++
+             fmt.Printf("%d\n", idx)
+             fmt.Printf("%s\n\n", s.Text())
+         })
+
+    doc.Find("#resultsList > ul:nth-child(3) > li:nth-child(1) > div:nth-child(2)").Each(func(idx int, s *goquery.Selection) {
+        s.Find("li").Each(func(idx int, s *goquery.Selection) {
+            fmt.Printf("%+v\n", s)
+        })
+    })
+
+}
+
+func main() {
+    if len(os.Args) != 2 {
+        os.Exit(1)
+    }
+    //fmt.Printf("args: %s", os.Args[1])
+    Scrape(os.Args[1])
+}
+oke-office-vm00-dev%
+
+*/
--- internal/apps/alm/alm_view.go
+++ internal/apps/alm/alm_view.go
@@ -1,12 +1,18 @@
 package alm_view
 
 import (
-    "fmt"
+	"fmt"
 
-    "../../../pkg/alm_common"
+	"../../../pkg/alm_common"
 )
 
-func Display_month(month alm_common_type.Month) string {
-    fmt.Print(month)
-    return "ok"
+func Display_month(months []alm_common_type.Month) string {
+
+	for idx, elt := range months {
+		fmt.Print(elt)
+		fmt.Print("\n")
+		fmt.Print("idx: %d", idx)
+	}
+
+	return "ok"
 }
